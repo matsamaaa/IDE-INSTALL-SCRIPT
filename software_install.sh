e8985e2789dd740ed198f1059baa421edc3ae50b
mat_sama <mateosejourne@gmail.com>
2023-12-13 14:28:21 +0100

    test update vscode download

--- software_install.sh
+++ software_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Télécharge le fichier .deb de Visual Studio Code
-wget -O vscode.deb http://go.microsoft.com/fwlink/?LinkID=760868
+wget -O vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 
 # Installe le paquet Visual Studio Code
 sudo dpkg -i vscode.deb
@@ -12,23 +12,23 @@ sudo apt install -f
 # Supprime le fichier .deb téléchargé
 rm vscode.deb
 
-# Spécifie la version de PHPStorm à télécharger (remplacez le lien avec la version correcte)
-phpstorm_version="2023.1.1"
-
-# Télécharge le fichier .tar.gz de PHPStorm
-wget -O phpstorm.tar.gz "https://download.jetbrains.com/webide/PhpStorm-${phpstorm_version}.tar.gz"
-
-# Extrait l'archive dans le répertoire /opt
-sudo tar xf phpstorm.tar.gz -C /opt/
-
-# Supprime l'archive téléchargée
-rm phpstorm.tar.gz
-
-# Ajoute le chemin de PHPStorm au fichier de configuration de l'environnement
-echo "export PATH=\"/opt/PhpStorm-${phpstorm_version}/bin/:$PATH\"" >> ~/.bashrc
-
-# Charge les modifications dans la session en cours
-source ~/.bashrc
-
-# Lance PHPStorm
-phpstorm
+## Spécifie la version de PHPStorm à télécharger (remplacez le lien avec la version correcte)
+#phpstorm_version="2023.1.1"
+#
+## Télécharge le fichier .tar.gz de PHPStorm
+#wget -O phpstorm.tar.gz "https://download.jetbrains.com/webide/PhpStorm-${phpstorm_version}.tar.gz"
+#
+## Extrait l'archive dans le répertoire /opt
+#sudo tar xf phpstorm.tar.gz -C /opt/
+#
+## Supprime l'archive téléchargée
+#rm phpstorm.tar.gz
+#
+## Ajoute le chemin de PHPStorm au fichier de configuration de l'environnement
+#echo "export PATH=\"/opt/PhpStorm-${phpstorm_version}/bin/:$PATH\"" >> ~/.bashrc
+#
+## Charge les modifications dans la session en cours
+#source ~/.bashrc
+#
+## Lance PHPStorm
+#phpstorm
