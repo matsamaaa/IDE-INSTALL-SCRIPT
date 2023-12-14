4cf31d7dac7d85516c971b612a5c57f08d49abc2
mat_sama <mateosejourne@gmail.com>
2023-12-14 11:47:47 +0100

    final update code

--- software_install.sh
+++ software_install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-## Télécharge le fichier .deb de Visual Studio Code
-#wget -O vscode.deb https://vscode.download.prss.microsoft.com/dbazure/download/stable/af28b32d7e553898b2a91af498b1fb666fdebe0c/code_1.85.0-1701895805_arm64.deb
-#
-## Installe le paquet Visual Studio Code
-#sudo dpkg -i vscode.deb
-#
-## Installe les dépendances manquantes
-#sudo apt install -f
-#
-## Supprime le fichier .deb téléchargé
-#rm vscode.deb
+# Télécharge le fichier .deb de Visual Studio Code
+wget -O vscode.deb https://vscode.download.prss.microsoft.com/dbazure/download/stable/af28b32d7e553898b2a91af498b1fb666fdebe0c/code_1.85.0-1701895805_arm64.deb
 
+# Installe le paquet Visual Studio Code
+sudo dpkg -i vscode.deb
+
+# Installe les dépendances manquantes
+sudo apt install -f
+
+# Supprime le fichier .deb téléchargé
+rm vscode.deb
+
+# Download chromium (chrome indisponible en arm64)
 sudo apt install chromium-browser
 
-echo "Visual Studio Code has been installed !"
+echo "Visual Studio & Chromium Code has been installed !"
