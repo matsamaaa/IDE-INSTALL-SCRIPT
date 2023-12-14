63eadecc9f61e460185a9296b7f75593707eda2f
mat_sama <mateosejourne@gmail.com>
2023-12-14 11:10:06 +0100

    update error arm

--- software_install.sh
+++ software_install.sh
@@ -13,7 +13,7 @@
 #rm vscode.deb
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_arm64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
 
 echo "Visual Studio Code has been installed !"
