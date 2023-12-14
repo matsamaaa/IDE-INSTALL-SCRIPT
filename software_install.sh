7a1f10fc0e64ed649d9e48bccf27f2d2c6484ec1
mat_sama <mateosejourne@gmail.com>
2023-12-14 11:31:39 +0100

    update install chromium

--- software_install.sh
+++ software_install.sh
@@ -12,8 +12,6 @@
 ## Supprime le fichier .deb téléchargé
 #rm vscode.deb
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+sudo apt install chromium-browser
 
 echo "Visual Studio Code has been installed !"
